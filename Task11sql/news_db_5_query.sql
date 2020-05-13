e80448e974fcfdbad5ff631b6ccd9593d0dc7f67
SergeyDorozhko <sergdorozhko@gmail.com>
2020-05-13 16:00:37 +0300

    done point 5. task 11 finished

--- Task11sql/news_db_5_query.sql
+++ Task11sql/news_db_5_query.sql
@@ -1,29 +1,34 @@
-
-SELECT t1.name, t1.surname, NTILE(2) OVER (ORDER BY newid DESC) as half FROM
-(
-SELECT id, name, surname, sys_guid() as newid
-from author
-) t1, 
-(SELECT count(*) as total from author) t2
-
-
---ORDER by newid
-;
-
-
-
-SELECT t1.name, NTILE(2) OVER (ORDER BY newid DESC) FROM
-(
-SELECT id, name, surname, sys_guid() as newid
-from author
-) t1, 
-(SELECT count(*) as total from author) t2
---ORDER by newid
-
-;
-
-
-select t1.name nae, t2.name name2 From author t1
-cross join author t2
-WHERE t2.id <> t1.id
-;
+/*
+5. * Make a writers competition cross-map. Create a statement that will generate random
+authors distribution by competition pairs.
+Statement will generate a list of author names pairs selected for a single round of
+tournament, displayed as two separate columns. Each author must be presented in
+resulting set (in both columns) once only. (The total number of authors must be even. Do
+not use custom functions during current implementation.)
+*/
+WITH competitors_list AS 
+    (
+    SELECT 
+        author_randomId_totalcount.name,
+        author_randomId_totalcount.surname,
+        NTILE((author_randomId_totalcount.totalAuthors)) OVER (partition by author_randomId_totalcount.totalAuthors ORDER BY newid DESC) as competitor_number,
+        author_randomId_totalcount.totalAuthors 
+    FROM 
+        (
+        SELECT   
+            name,
+            surname,
+            sys_guid() as newid,
+            totalAuthors
+        FROM
+            author,
+            (SELECT COUNT(*) AS totalAuthors from author) ) author_randomId_totalcount)
+SELECT  
+    one.name,
+    one.surname,
+    'VS' , 
+    two.name, 
+    two.surname 
+FROM competitors_list one, competitors_list two
+WHERE one.competitor_number = two.competitor_number+(two.totalAuthors/2)
+ORDER BY one.competitor_number;
